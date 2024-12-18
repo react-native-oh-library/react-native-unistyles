49cbd171109ca69c84759bf82ca5e9ae7e3cb2f1
severnking <2523913042@qq.com>
2024-12-18 16:18:52 +0800

    chore: 开源五项核查整改

--- harmony/unistyles/src/main/ets/Insets.ts
+++ harmony/unistyles/src/main/ets/Insets.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import common from '@ohos.app.ability.common';
 import { SafeAreaInsets } from '@rnoh/react-native-openharmony/ts';
 
--- harmony/unistyles/src/main/ets/RNUnistylesTurboModule.ts
+++ harmony/unistyles/src/main/ets/RNUnistylesTurboModule.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { TurboModule,TurboModuleContext } from '@rnoh/react-native-openharmony/ts';
 import window from '@ohos.window';
 import common from '@ohos.app.ability.common'
